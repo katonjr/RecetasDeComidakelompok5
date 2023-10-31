8654a07fc951e80118d87fdd857ec8d0a3690895
Katonjr <Ciplukml80@gmail.com>
2023-10-31 23:40:26 +0700

    Membuat halaman activity signup

--- app/src/main/java/com/example/recetasdecomida/login.kt
+++ app/src/main/java/com/example/recetasdecomida/login.kt
@@ -1,11 +1,19 @@
 package com.example.recetasdecomida
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class login : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var signin: Button=findViewById(R.id.buttonkesignup)
+        signin.setOnClickListener({val intent = Intent(this, signup::class.java)
+            startActivity(intent)})
+
     }
 }
--- app/src/main/java/com/example/recetasdecomida/signup.kt
+++ app/src/main/java/com/example/recetasdecomida/signup.kt
@@ -1,11 +1,17 @@
 package com.example.recetasdecomida
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        var loginn: Button =findViewById(R.id.buttonkelogin)
+        loginn.setOnClickListener({val intent = Intent(this, login::class.java)
+            startActivity(intent)})
     }
 }
