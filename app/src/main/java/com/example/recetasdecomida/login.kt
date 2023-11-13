e81c81ceb5e7ef3b8c054783232f17a117e47b44
vithaa <desvithasksk@gmail.com>
2023-11-13 14:08:30 +0700

    menghubungkan login akun firebase

--- app/src/main/java/com/example/recetasdecomida/login.kt
+++ app/src/main/java/com/example/recetasdecomida/login.kt
@@ -1,15 +1,51 @@
 package com.example.recetasdecomida
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class login : AppCompatActivity() {
+    lateinit var editEmail: EditText
+    lateinit var editPassword: EditText
+    lateinit var btnLogin: Button
+    lateinit var  firebase: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        firebase = FirebaseAuth.getInstance()
+        editEmail = findViewById(R.id.emaillogin)
+        editPassword = findViewById(R.id.passwordlogin)
+        btnLogin = findViewById(R.id.buttonlogin)
+
+        btnLogin.setOnClickListener {
+            val getEmail = editEmail.text.toString()
+            val getPassword = editPassword.text.toString()
+            if (getEmail.isEmpty()){
+                Toast.makeText(this, "Email tidak boleh kosong", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (getPassword.isEmpty()){
+                Toast.makeText(this, "Password tidak boleh kosong", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            firebase.signInWithEmailAndPassword(getEmail,getPassword).addOnSuccessListener {
+                Toast.makeText(this, "Berhasil Login", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            }
+
+            firebase.signInWithEmailAndPassword(getEmail,getPassword).addOnFailureListener {
+                Toast.makeText(this, "Email atau Password Salah", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
 
         var signin: Button=findViewById(R.id.buttonkesignup)
         signin.setOnClickListener {
@@ -17,5 +53,12 @@ class login : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    private fun Validation(email:String,password:String,konteks:Context){
+        if (email.isEmpty() || password.isEmpty()){
+            Toast.makeText(konteks, "Email atau Password tidak boleh kosong", Toast.LENGTH_SHORT).show()
+            return
+        }
     }
 }
--- app/src/main/java/com/example/recetasdecomida/signup.kt
+++ app/src/main/java/com/example/recetasdecomida/signup.kt
@@ -1,21 +1,61 @@
 package com.example.recetasdecomida
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 
 class signup : AppCompatActivity() {
     lateinit var editEmail: EditText
+    lateinit var editPassword: EditText
+    lateinit var btnSignup: Button
+    lateinit var  firebase: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        firebase = FirebaseAuth.getInstance()
+        editEmail = findViewById(R.id.emailsignup)
+        editPassword = findViewById(R.id.passwordsignup)
+        btnSignup = findViewById(R.id.buttonsignup)
+
+
+
+        btnSignup.setOnClickListener {
+            val getEmail = editEmail.text.toString()
+            val getPassword = editPassword.text.toString()
+
+            setEmailAndPassword(getEmail,getPassword)
+
+            firebase.createUserWithEmailAndPassword(
+                getEmail,getPassword
+            ).addOnSuccessListener {
+                val intent = Intent(this,login::class.java)
+                startActivity(intent)
+            }
+        }
+
+
+
 
 
         var loginn: Button =findViewById(R.id.buttonkelogin)
         loginn.setOnClickListener({val intent = Intent(this, login::class.java)
             startActivity(intent)})
+    }
+
+   fun setEmailAndPassword(email: String,password: String){
+       if (email.isEmpty() || password.isEmpty()){
+           return
+       }
+
+
+
+
     }
 }
